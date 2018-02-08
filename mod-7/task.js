d47690e5dc0c23421ec9c74f0bd7090ced441984
Shmaiser26 <oleksiy.plysak@gmail.com>
2018-02-08 12:36:48 +0200

    need to fix

--- mod-7/task.js
+++ mod-7/task.js
@@ -1,35 +1,38 @@
 const body = document.querySelector('body');
 body.classList.add("main");
 
+const container = document.createElement('div');
+container.classList.add('container');
+
 const user_card = document.createElement('div');
-user_card
-    .classList
-    .add("user-card");
+user_card.classList.add('user-card');
 
-const user_foto = document.createElement('img');
-user_foto
-    .classList
-    .add("user-photo");
+const image = document.createElement('img');
+image.classList.add('image');
 
-const user_name = document.createElement('span');
-user_name
-    .classList
-    .add("name");
-user_name.textContent = "Claudia Cardinale";
+const name = document.createElement('span');
+name.classList.add('text');
 
-const user_info = document.createElement('span');
-user_info
-    .classList
-    .add("adress");
-user_info.textContent = "29 Philadellphia, PA";
+const adress = document.createElement('span');
+adress.classList.add('text');
 
-const user_icons_block = document.createElement('ul');
-const user_icons_block_icon = document.createElement('li');
-body.append(user_card);
-user_card.append(user_foto);
-user_card.append(user_name);
-user_card.append(user_info);
-user_card.append(user_icons_block);
-user_icons_block.append(user_icons_block_icon);
+const icons_block = document.createElement('ul');
+icons_block.classList.add('icons-block');
 
+const icon_a = document.createElement('li');
+icon_a.classList.add('icons');
+icon_a.classList.add('icon-a::before');
+
+const icon_b = document.createElement('li');
+icon_b.classList.add('icons');
+icon_b.classList.add('icon-b::before');
+
+const icon_c = document.createElement('li');
+icon_c.classList.add('icons');
+icon_c.classList.add('icon-c::before');
+
+body.appendChild(container);
+container.appendChild(user_card);
+user_card.appendChild(image, name, adress, icons_block);
+icons_block.append(icon_a, icon_b, icon_c);
 console.log(body);
